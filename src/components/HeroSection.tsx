4a6a95151b6c3f299987785bc3538f6a7ed1bb87
emre erdem <emrerdem94@gmail.com>
2021-05-06 20:52:58 +0300

    Set basic search handler

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HeroSection.scss';
 import LOGO_PATH from '../assets/media/gameforge-logo.png';
 import SEARCH_ICON_PATH from '../assets/media/search.svg';
 import CLOSE_ICON_PATH from '../assets/media/close.svg';
 import * as BACKGROUND_VIDEO_PATH from '../assets/media/gameforge-video.mp4';
 
-export const HeroSection: React.FC = () => {
+export const HeroSection: React.FC = (): JSX.Element => {
+  const [searchInput, setSearchInput] = useState<string>('');
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchInput(e.target.value);
+  };
+
   return (
     <div className="hero-container">
       <header className="header">
@@ -20,14 +26,16 @@ export const HeroSection: React.FC = () => {
         </div>
       </header>
       <div className="search">
-        <video
-          id="gameforgeVideo"
-          playsInline={true}
-          autoPlay={true}
-          loop={true}
-          muted={true}
-          src={BACKGROUND_VIDEO_PATH.default}
-        />
+        <div className="video-wrapper">
+          <video
+            id="gameforgeVideo"
+            playsInline={true}
+            autoPlay={true}
+            loop={true}
+            muted={true}
+            src={BACKGROUND_VIDEO_PATH.default}
+          />
+        </div>
         <h2>Lorem ipsum dolor sit amet consectetur</h2>
         <p>
           With the Cloud Gaming, you can join, play, and share games online with
@@ -39,12 +47,27 @@ export const HeroSection: React.FC = () => {
             src={SEARCH_ICON_PATH}
             alt="search icon"
           />
-          <input type="text" placeholder="Search Games" />
-          <img
-            className="search--bar__close"
-            src={CLOSE_ICON_PATH}
-            alt="close icon"
+          <input
+            type="text"
+            placeholder="Search Games"
+            onChange={handleSearch}
+            value={searchInput}
           />
+          {searchInput && (
+            <>
+              <a
+                className="search--bar__close"
+                onClick={() => setSearchInput('')}
+              >
+                <img
+                  className="search--bar__close-icon"
+                  src={CLOSE_ICON_PATH}
+                  alt="close icon"
+                />
+              </a>
+              <div className="searchPreview"></div>
+            </>
+          )}
         </div>
       </div>
     </div>
