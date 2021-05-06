307d243a8539142d682108e52662945adae003a2
emre erdem <emrerdem94@gmail.com>
2021-05-06 19:41:31 +0300

    Implement right panel in browse section

--- src/components/BrowseSection.tsx
+++ src/components/BrowseSection.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './BrowseSection.scss';
+import COLLAPSE_ICON from '../assets/media/collapse-arrow.svg';
+import POLYGON from '../assets/media/polygon.png';
 
 export const BrowseSection: React.FC = () => {
   return (
@@ -72,11 +74,29 @@ export const BrowseSection: React.FC = () => {
         </div>
       </div>
       <div className="games">
-        <div className="dropdown">Title A-Z</div>
+        <div className="dropdown">
+          <div className="button-wrapper">
+            <button className="dropdown-button">Title A-Z</button>
+            <img src={COLLAPSE_ICON} alt="collapse icon" />
+          </div>
+          <div className="dropdown-items">
+            <a href="#">Title A-Z</a>
+            <a href="#">Title Z-A</a>
+          </div>
+        </div>
         <div className="game-list">
           <div className="game-list--item">
-            <div className="game-list--item_letter"></div>
+            <div className="game-list--item_letter">
+              <span>A</span>
+              <img src={POLYGON} alt="green polygon" />
+            </div>
             <div className="game-list--item_bullets">
+              <p>Lorem ipsum dolor sit</p>
+              <p>Adipiscing consectetur dolor</p>
+              <p>Consectetur lorem adipiscing</p>
+              <p>Adipiscing consectetur dolor</p>
+              <p>Consectetur lorem adipiscing</p>
+              <p>Adipiscing consectetur dolor</p>
               <p>Lorem ipsum dolor sit</p>
               <p>Adipiscing consectetur dolor</p>
               <p>Consectetur lorem adipiscing</p>
