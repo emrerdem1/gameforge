65c7891f7dcf3a87c6c7ce53e2dc9153a5dc2197
emre erdem <emrerdem94@gmail.com>
2021-05-06 04:59:58 +0300

    Delete redundant files and build the structure

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HeroSection.scss';
+
+export const HeroSection: React.FC = () => {
+  return (
+    <div className="hero-container">
+      <header className="header"></header>
+      <div className="search"></div>
+    </div>
+  );
+};
